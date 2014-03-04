d64b1bffa917ac8d7ac18cbdd86307911f806561
Gabriel Aguilera <gabrieag@umich.edu>
2014-03-04 04:14:45 -0500

    Initial Commit

---
+++ Mans Best Friend/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mans Best Friend
+//
+//  Created by Gabriel Aguilera on 3/4/14.
+//  Copyright (c) 2014 Gabriel Aguilera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GAAppDelegate class]));
+    }
+}
