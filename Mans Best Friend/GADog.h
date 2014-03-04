27224c1208446e96ea079f7c2a74de286a98b3c9
Gabriel Aguilera <gabrieag@umich.edu>
2014-03-04 04:33:23 -0500

    Initial commit

---
+++ Mans Best Friend/GADog.h
@@ -0,0 +1,21 @@
+//
+//  GADog.h
+//  Mans Best Friend
+//
+//  Created by Gabriel Aguilera on 3/4/14.
+//  Copyright (c) 2014 Gabriel Aguilera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GADog : NSObject
+
+@property (nonatomic) int age;
+
+@property (nonatomic, strong) NSString *breed;
+
+@property (nonatomic, strong) UIImage *image;
+
+
+
+@end
---
+++ Mans Best Friend/GADog.m
@@ -0,0 +1,13 @@
+//
+//  GADog.m
+//  Mans Best Friend
+//
+//  Created by Gabriel Aguilera on 3/4/14.
+//  Copyright (c) 2014 Gabriel Aguilera. All rights reserved.
+//
+
+#import "GADog.h"
+
+@implementation GADog
+
+@end
