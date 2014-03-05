44ff37f462bac2aa19f69d02aa3aebc8e1deb867
Gabriel Aguilera <gabrieag@umich.edu>
2014-03-05 03:02:52 -0500

    Added toolbar to storyboard, pressing label shows new dogs.

--- Mans Best Friend/GAViewController.h
+++ Mans Best Friend/GAViewController.h
@@ -11,11 +11,14 @@
 @interface GAViewController : UIViewController
 
 -(void)printHelloWorld;
+- (IBAction)newDogBarButtonItemPressed:(UIBarButtonItem *)sender;
 
 @property (strong, nonatomic) IBOutlet UIImageView *myImageView;
 
 @property (strong, nonatomic) IBOutlet UILabel *nameLabel;
 
 @property (strong, nonatomic) IBOutlet UILabel *breedLabel;
+
+@property (strong, nonatomic) NSMutableArray *myDogs;
 
 @end
--- Mans Best Friend/GAViewController.m
+++ Mans Best Friend/GAViewController.m
@@ -29,6 +29,34 @@
     self.nameLabel.text = myDog.name;
     self.breedLabel.text = myDog.breed;
     
+    
+    GADog *secondDog = [[GADog alloc] init];
+    secondDog.name = @"Wishbone";
+    secondDog.breed = @"Jack Russell Terrier";
+    secondDog.image = [UIImage imageNamed:@"JRT.jpg"];
+    
+    GADog *thirdDog = [[GADog alloc] init];
+    thirdDog.name = @"Lassie";
+    thirdDog.breed = @"Collie";
+    thirdDog.image = [UIImage imageNamed:@"BorderCollie.jpg"];
+    
+    GADog *fourthDog = [[GADog alloc] init];
+    fourthDog.name = @"Angel";
+    fourthDog.breed = @"Greyhound";
+    fourthDog.image = [UIImage imageNamed:@"ItalianGreyhound.jpg"];
+    
+    //Allocate space for the member
+    self.myDogs = [[NSMutableArray alloc] init];
+    
+    //Use an instance method to add the objects.
+    //Note addObject appends it to the end.
+    [self.myDogs addObject:myDog];
+    [self.myDogs addObject:secondDog];
+    [self.myDogs addObject:thirdDog];
+    [self.myDogs addObject:fourthDog];
+    NSLog(@"%@", self.myDogs);
+    
+    
     //[object method]
     //[myDog barkANumberOfTimes:2];
     
@@ -39,7 +67,7 @@
     //NSLog(@"%@", myDog.breed);
     
     
-    [self printHelloWorld];
+    //[self printHelloWorld];
     
 }
 
@@ -52,6 +80,24 @@
 -(void)printHelloWorld
 {
     NSLog(@"Hello World!");
+}
+
+- (IBAction)newDogBarButtonItemPressed:(UIBarButtonItem *)sender
+{
+    //Get the number of dogs in the array using the instance method
+    int numberOfDogs = [self.myDogs count];
+    
+    //arc4random is a pseudorandom generator
+    int randomIndex = arc4random() % numberOfDogs;
+    
+    GADog *randomDog = [self.myDogs objectAtIndex:randomIndex];
+    
+    self.myImageView.image = randomDog.image;
+    self.breedLabel.text = randomDog.breed;
+    self.nameLabel.text = randomDog.name;
+    
+    //Update the button that was pressed.
+    sender.title = @"And Another";
 }
 
 @end
